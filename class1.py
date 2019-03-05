d16d050bcf9977c58b702fe569430396977cfbd0
unknown <1020154356@qq.com>
2019-03-05 13:29:35 +0800

    first commit

---
+++ class1.py
@@ -0,0 +1,20 @@
+import requests
+url = "http://www.amazon.cn/gp/product/B01M8L5Z3Y"
+# def getHTMLText(url):
+#     try:
+#         r=requests.get(url,timeout=30)
+#         r.raise_for_status()
+#         r.encoding = r.apparent_encoding
+#         return r.text
+#     except:
+#         return "产生异常"
+# if __name__ == "__main__":
+#     url = "http://item.jd.com/2967929.html"
+#     print(getHTMLText(url))
+try:
+    r = requests.get(url)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text[:1000])
+except:
+    print("爬取失败")
